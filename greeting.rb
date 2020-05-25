c831c3528c0de73eedd8ebd8fa620a3a1ceb3799
Mario Terrazas <mtrrzs@hotmail.com>
2020-05-25 04:16:46 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -5,3 +5,18 @@ end
 greeting
 greeting
 
+def the_woman_I_love
+  phrase = "Maria Paula"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+the_woman_I_love
